e73a339dc8d5a1f21dab2c2d452c05c7db4d58f6
Chris Mittell <mittell@hotmail.co.uk>
2022-11-08 13:57:49 +0900

    feat: added tests with different assertions

--- src/components/TodoFooter/__tests__/TodoFooter.test.js
+++ src/components/TodoFooter/__tests__/TodoFooter.test.js
@@ -18,4 +18,44 @@ describe('<TodoFooter/>', () => {
 
 		expect(paragraphElement).toBeInTheDocument();
 	});
+
+	it('should render "task" when the number of incomplete tasks is 1', () => {
+		render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+
+		const paragraphElement = screen.getByText('1 task left');
+
+		expect(paragraphElement).toBeInTheDocument();
+	});
+
+	it('should render a paragraph element', () => {
+		render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+
+		const paragraphElement = screen.getByText('1 task left');
+
+		expect(paragraphElement).toContainHTML('p');
+	});
+
+	it('should render the correct text content', () => {
+		render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+
+		const paragraphElement = screen.getByTestId('para');
+
+		expect(paragraphElement).toHaveTextContent('5 tasks left');
+	});
+
+	it('should NOT render "task" when the number of incomplete tasks above 1', () => {
+		render(<MockTodoFooter numberOfIncompleteTasks={2} />);
+
+		const paragraphElement = screen.getByText('2 tasks left');
+
+		expect(paragraphElement).not.toBeFalsy();
+	});
+
+	it('should render the correct amount of incomplete tasks as text', () => {
+		render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+
+		const paragraphElement = screen.getByText('5 tasks left');
+
+		expect(paragraphElement.textContent).toBe('5 tasks left');
+	});
 });
