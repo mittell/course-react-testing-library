e246ab7f16f6dc649228219f8b533fc30326a3c2
Chris Mittell <mittell@hotmail.co.uk>
2022-11-08 13:20:18 +0900

    feat: added example tests

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,10 +1,66 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-it('should render <Header/> with title prop', () => {
-	render(<Header title='Todo' />);
+describe('GetBy Tests', () => {
+	it('should render <Header/> with title prop', () => {
+		render(<Header title='Todo' />);
 
-	const headingElement = screen.getByText('Todo');
+		const headingElement = screen.getByText('Todo');
 
-	expect(headingElement).toBeInTheDocument();
+		expect(headingElement).toBeInTheDocument();
+	});
+
+	it('should render <Header/> and header element with title "Todo"', () => {
+		render(<Header title='Todo' />);
+
+		const headingElement = screen.getByRole('heading', { name: 'Todo' });
+
+		expect(headingElement).toBeInTheDocument();
+	});
+
+	it('should render <Header/> and header element with title "Cat"', () => {
+		render(<Header title='Todo' />);
+
+		const headingElement = screen.getByTitle('Cat');
+
+		expect(headingElement).toBeInTheDocument();
+	});
+
+	it('should render <Header/> and header element with testid "header-1"', () => {
+		render(<Header title='Todo' />);
+
+		const headingElement = screen.getByTestId('header-1');
+
+		expect(headingElement).toBeInTheDocument();
+	});
+});
+
+describe('FindBy Tests', () => {
+	it('should render <Header/> with title prop', async () => {
+		render(<Header title='Todo' />);
+
+		const headingElement = await screen.findByText('Todo');
+
+		expect(headingElement).toBeInTheDocument();
+	});
+});
+
+describe('QueryBy Tests', () => {
+	it('should render <Header/> with title prop', () => {
+		render(<Header title='Todo' />);
+
+		const headingElement = screen.queryByText('Test');
+
+		expect(headingElement).not.toBeInTheDocument();
+	});
+});
+
+describe('GetAllBy Tests', () => {
+	it('should render <Header/> and all header elements', () => {
+		render(<Header title='Todo' />);
+
+		const headingElements = screen.getAllByRole('heading');
+
+		expect(headingElements.length).toBe(2);
+	});
 });
